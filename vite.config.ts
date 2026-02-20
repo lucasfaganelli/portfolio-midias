cc4bce8db43dbab5736b6e1c700aa0b0a813b03d
Lucas Faganelli <lucasfaganelli@gmail.com>
2026-02-20 11:34:41 -0300

    fix: removendo react router

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/portfolio-midias/" : "/",
+export default defineConfig({
+  base: "/portfolio-midias/",
 
   server: {
     host: "::",
@@ -14,11 +14,11 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), componentTagger()].filter(Boolean),
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
