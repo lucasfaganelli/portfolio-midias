d153ae0b6e864f9c1c55c88de45add31b35b526b
Lucas Faganelli <lucasfaganelli@gmail.com>
2026-02-19 22:30:06 -0300

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,9 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+import { HashRouter } from "react-router-dom";
+
+<HashRouter>
+  <App />
+</HashRouter>
